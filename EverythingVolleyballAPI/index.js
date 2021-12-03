883286cbd3c1a318e7e360d03fe2a969cd7ba20d
Dustin Flynn <flynnd297@gmail.com>
2021-12-03 06:31:25 -0600

    added order routes and git ignore.

--- EverythingVolleyballAPI/index.js
+++ EverythingVolleyballAPI/index.js
@@ -5,7 +5,8 @@ const dotenv = require("dotenv");
 const userRoute = require("./routes/user")
 const authenticationRoute = require("./routes/authentication")
 const productRoute = require("./routes/product")
-const cartRoute = require("./routes/product")
+const cartRoute = require("./routes/cart")
+const orderRoute = require("./routes/order")
 
 
 dotenv.config();
@@ -21,7 +22,8 @@ app.use(express.json())
 app.use("/api/auth", authenticationRoute)
 app.use("/api/users", userRoute)
 app.use("/api/products", productRoute)
-app.use("/api/cart", cartRoute)
+app.use("/api/carts", cartRoute)
+app.use("/api/orders", orderRoute)
 
 app.listen(process.env.PORT || 5000, ()=>{
     console.log("Backend server is running!")
