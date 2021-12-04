7634353a08ab641c109f4d23e9d5482a8c7eec4f
Dustin Flynn <flynnd297@gmail.com>
2021-12-04 17:18:10 -0600

    Changed the .gitignore files and added .env to the client side folders. Added an api call file for Redux.

--- everything-volleyball/src/requestMethods.js
+++ everything-volleyball/src/requestMethods.js
@@ -1,12 +1,19 @@
-import axios from 'axios'
+import axios from "axios";
 
-const BASE_URL = "http://localhost:5000/api"
-const TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYxYTlmOTAwYjE5YTQ0Nzc3NGJmOGU3YyIsImlzQWRtaW4iOnRydWUsImlhdCI6MTYzODU2MzY4OSwiZXhwIjoxNjM5MTY4NDg5fQ.raoOB6GtWgpUkaP_iYWBay4DKTqvnhG78mHNpSJB-BA"
+const BASE_URL = "http://localhost:5000/api/";
+// const TOKEN =
+//   JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user).currentUser
+//     .accessToken || "";
+
+const user = JSON.parse(localStorage.getItem("persist:root"))?.user;
+const currentUser = user && JSON.parse(user).currentUser;
+const TOKEN = currentUser?.accessToken;
 
 export const publicRequest = axios.create({
-    baseURL : BASE_URL
+  baseURL: BASE_URL,
 });
+
 export const userRequest = axios.create({
-    baseURL : BASE_URL,
-    header:{token:`Bearer ${TOKEN}`}
+  baseURL: BASE_URL,
+  header: { token: `Bearer ${TOKEN}` },
 });
